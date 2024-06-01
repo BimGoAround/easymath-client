19df915fb3116147cb9bdb16b61e0a6fb9e31a5f
Toan <84128650+toan2013dn@users.noreply.github.com>
2024-06-01 20:23:10 +0700

    fix: format file (#2)

--- src/components/HomeContent/index.tsx
+++ src/components/HomeContent/index.tsx
@@ -1,12 +1,12 @@
 export const HomeContent = () => {
-    return (
-        <>
-        <span className= "font-bold text-5xl leading-10" >
+  return (
+    <>
+      <span className="font-bold text-5xl leading-10">
         Photo, Formula, and Answer
-            < /span>
-            < span className = "font-normal text-base leading-7 mt-3" >
-                Take a photo, describe the problem, submit and get answer within seconds
-                    < /span>
-                    < />
+      </span>
+      <span className="font-normal text-base leading-7 mt-3">
+        Take a photo, describe the problem, submit and get answer within seconds
+      </span>
+    </>
   );
 };
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,7 +1,7 @@
 import {
-    ArrowRightIcon,
-    CrossCircledIcon,
-    ImageIcon,
+  ArrowRightIcon,
+  CrossCircledIcon,
+  ImageIcon,
 } from '@radix-ui/react-icons';
 import { useState } from 'react';
 
@@ -9,70 +9,70 @@ import { Button } from '../ui/button';
 import { Textarea } from '../ui/textarea';
 
 export const Input = () => {
-    const [image, setImage] = useState<string | undefined>();
-    const [input, setInput] = useState<string>('');
+  const [image, setImage] = useState<string | undefined>();
+  const [input, setInput] = useState<string>('');
 
-    const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (event.target.files && event.target.files[0]) {
-            const img = event.target.files[0];
-            setImage(URL.createObjectURL(img));
-        }
-    };
+  const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      const img = event.target.files[0];
+      setImage(URL.createObjectURL(img));
+    }
+  };
 
-    const onRemoveImage = () => {
-        setImage(undefined);
-    };
+  const onRemoveImage = () => {
+    setImage(undefined);
+  };
 
-    const onInputChange = (event: any) => {
-        setInput(event.target.value);
-    };
-    return (
-        <>
-            <Textarea
-                className="bg-zinc-700 text-zinc-400 resize-none rounded-t-2xl outline-none"
-                placeholder={'Type your description here...'}
-                value={input}
-                onChange={onInputChange}
-            />
+  const onInputChange = (event: any) => {
+    setInput(event.target.value);
+  };
+  return (
+    <>
+      <Textarea
+        className="bg-zinc-700 text-zinc-400 resize-none rounded-t-2xl outline-none"
+        placeholder={'Type your description here...'}
+        value={input}
+        onChange={onInputChange}
+      />
 
-            <div className="bg-zinc-700 p-3 rounded-b-2xl flex items-end justify-between">
-                <div>
-                    {image && (
-                        <div className="w-8 h-8 relative">
-                            <div className=" w-full h-full overflow-hidden rounded relative">
-                                <img className="w-full h-full object-cover" src={image} />
-                            </div>
-                            <CrossCircledIcon
-                                className="absolute -top-1 -right-1 text-zinc-400 w-3 h-3 z-10 cursor-pointer"
-                                onClick={onRemoveImage}
-                            />
-                        </div>
-                    )}
-                    <input
-                        type="file"
-                        name="file"
-                        id="file"
-                        className="opacity-0 overflow-hidden w-0 h-0"
-                        accept="image/png, image/gif, image/jpeg"
-                        onChange={onImageChange}
-                    />
-                    <label
-                        htmlFor="file"
-                        className="flex items-center gap-1 text-xs text-zinc-100 cursor-pointer"
-                    >
-                        <ImageIcon />
-                        <span>Image</span>
-                    </label>
-                </div>
-                <Button
-                    variant={'link'}
-                    size={'sm'}
-                    disabled={!input ? true : false}
-                    className="hover:bg-zinc-900 rounded"
-                >
-                    <ArrowRightIcon className="text-zinc-100" />
-                </Button>
+      <div className="bg-zinc-700 p-3 rounded-b-2xl flex items-end justify-between">
+        <div>
+          {image && (
+            <div className="w-8 h-8 relative">
+              <div className=" w-full h-full overflow-hidden rounded relative">
+                <img className="w-full h-full object-cover" src={image} />
+              </div>
+              <CrossCircledIcon
+                className="absolute -top-1 -right-1 text-zinc-400 w-3 h-3 z-10 cursor-pointer"
+                onClick={onRemoveImage}
+              />
             </div>
-        </>
-    );
+          )}
+          <input
+            type="file"
+            name="file"
+            id="file"
+            className="opacity-0 overflow-hidden w-0 h-0"
+            accept="image/png, image/gif, image/jpeg"
+            onChange={onImageChange}
+          />
+          <label
+            htmlFor="file"
+            className="flex items-center gap-1 text-xs text-zinc-100 cursor-pointer"
+          >
+            <ImageIcon />
+            <span>Image</span>
+          </label>
+        </div>
+        <Button
+          variant={'link'}
+          size={'sm'}
+          disabled={!input ? true : false}
+          className="hover:bg-zinc-900 rounded"
+        >
+          <ArrowRightIcon className="text-zinc-100" />
+        </Button>
+      </div>
+    </>
+  );
 };
