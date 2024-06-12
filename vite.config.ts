aebdf77864f5c312369cb3ec9f68d4e66536000b
TienThinh-fe <tienthinhhoang317@gmail.com>
2024-06-12 17:01:57 +0700

    chore: update vite config for env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env.VITE_API_BASE_URL': JSON.stringify(
+      process.env.VITE_API_BASE_URL,
+    ),
+  },
   plugins: [react(), svgr()],
   resolve: {
     alias: {
